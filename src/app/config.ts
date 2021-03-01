9b62a7d950bdacf34400db03aed3497abd555ee5
gilsan <jhhan@allaboutsip.co.kr>
2021-03-01 16:24:57 +0900

    design change

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,8 +1,9 @@
 // private apiUrl = 'http://160.1.17.79:3000';  // EMR 서버
 // private apiUrl = 'http://112.169.53.30:3000';
 
-// export const emrUrl = 'http://160.1.17.79:3000';
 export const emrUrl = 'http://112.169.53.30:3000';
+
+ //export const emrUrl = 'http://localhost:3000';
 // export const emrUrl = 'http://160.1.17.79:3000';
 // export const emrUrl = 'http://160.1.17.79:5000';
 //
